27172b3d1091dc857ab17427408e54e49ad3bf07
sunxu <sunxu42@outlook.com>
2021-11-15 02:37:30 +0800

    启迈项目

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QimaComponent } from './qimaQA/qima.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QimaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/qimaQA/qima.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'qima',
+  templateUrl: './qima.component.html',
+  styleUrls: ['./qima.component.css']
+})
+export class QimaComponent {
+}
